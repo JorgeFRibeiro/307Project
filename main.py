bb76b1258b7e5ad8cc0e90612786f18ab8ed4b6b
cdvindhya <bandav@purdue.edu>
2022-02-23 16:22:16 -0500

    UI additions for Edit Profile page.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import BinOp
 import re
 from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import login_required, current_user
@@ -18,6 +19,12 @@ def index():
 def profile():
     return render_template('profile.html', name = current_user.name, bio = current_user.bio)
 
+# Edit Profile Page
+@main.route('/edit_profile')
+@login_required
+def edit_profile():
+    return render_template('edit_profile.html', name = current_user.name, bio = current_user.bio)
+
 # Account delete and other functions, differentiated based on 'action's value
 @main.route('/profile', methods=['POST'])
 @login_required
@@ -31,7 +38,6 @@ def profile_delete():
 
     # TODO: Other buttons if they exist
     return redirect(url_for('main.profile'))
-
 
 # Post related functions
 # TODO: Complete once front end components are added
@@ -48,6 +54,43 @@ def create_post():
     # TODO: Display some sort of post creation message
     return redirect(url_for('main.profile'))
 
+# Access the Edit Profile page
+@main.route('/edit_profile', methods=['GET'])
+@login_required
+def show_edit_profile():
+    return redirect(url_for('main.edit_profile'))
+
+# Functionality to update a user's profile
+@main.route('/edit_profile', methods=['POST'])
+@login_required
+def update_profile():
+    if request.form.get('action') == "Edit Profile":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        name = request.form.get('name')
+        bio = request.form.get('bio')
+
+        if not email:
+            email = current_user.email
+        if not password:
+            password = current_user.password
+        if not name:
+            name = current_user.name
+        if not bio:
+            bio = current_user.bio
+
+        user = User.query.filter_by(email=email).first() 
+        print(user)
+        user.email = email
+        user.password = password
+        user.name = name
+        user.bio = bio
+        print("REACHED")
+        #user = User(email=email, name=name, password=generate_password_hash(password, method='sha256')) 
+        db.session.merge(user)
+        db.session.flush()
+        db.session.commit()
+    return redirect(url_for('main.profile'))
 
 @login_required
 def delete_post():
