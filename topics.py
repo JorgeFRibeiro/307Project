bf1e406bdc8c49c286544e6aff68e6889ad8dd62
Bkim057 <kim3224@purdue.edu>
2022-04-19 20:06:03 -0400

    Added support and links to the available topics page

---
+++ topics.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, redirect, render_template, request, url_for, flash
+from flask_login import login_required, current_user
+from .models import User, Post, Topic
+from . import db
+
+topics = Blueprint('topics', __name__)
+
+# Gets a list of all topics
+def existing_topics():
+    topic_list = []
+    for topic_name in Topic.query(Topic.name).distinct():
+        topic_list.append(topic_name)
+    return topic_list
+
+# Convert topic to html for box in all-topics page
+def topic_to_html(topic):
+    # html conversion
+    html_string =  "<div class=\"box\">\
+                        <h3>" + str(topic) + "</h3>\
+                        <form action=\"/view_topic/" + str(topic) + "\">\
+                            <button>View this Topic</button>\
+                        </form>\
+                    </div>"
+    return html_string
+
+# Display all currently available topics
+@topics.route('/all_topics_page')
+def all_topics_page():
+    topics = existing_topics()
+    topics_html_string = ""
+    if len(topics) == 0:
+        topics_html_string += "<div class=\"box\">\
+                                    <h3>No topics exist at the moment!</h3>\
+                                </div>"
+    else:
+        for cur_topic in topics:
+            topics_html_string += topic_to_html(cur_topic)
+    return render_template('topics_page.html', topics_string=topics_html_string)
+
+# Display all posts under a topic
+@topics.route('/view_topic/<topic>')
+def view_topic():
+    return render_template('index.html')
